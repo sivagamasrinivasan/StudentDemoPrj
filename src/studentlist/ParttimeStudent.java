b49942b5d6eb1393bb57853bea853e1b47105623
sivagamasrinivasan <784108@pdsb.net>
2025-09-18 17:31:26 -0400

    added parttime

---
+++ src/studentlist/ParttimeStudent.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author kumar
+ */
+public class ParttimeStudent {
+    private int sid;
+    private int numofhrs;
+
+    /**
+     * @return the sid
+     */
+    public int getSid() {
+        return sid;
+    }
+
+    /**
+     * @param sid the sid to set
+     */
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    /**
+     * @return the numofhrs
+     */
+    public int getNumofhrs() {
+        return numofhrs;
+    }
+
+    /**
+     * @param numofhrs the numofhrs to set
+     */
+    public void setNumofhrs(int numofhrs) {
+        this.numofhrs = numofhrs;
+    }
+}
